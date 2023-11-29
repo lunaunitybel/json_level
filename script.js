d342f1ab5156323be086b01f38300764ff17cb21
Ignacio Caravia <caravia@gmail.com>
2023-11-29 22:27:01 +0100

    Typo bug fixed

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function generateJSON() {
     const level_version = document.getElementById('level_version').value;
     const ordinal = document.getElementById('ordinal').value;
     const moves = document.getElementById('moves').value;
-    const objectives_blocker1 = document.getElementById('objectives_blocker1').value;
-    const objectives_blocker2 = document.getElementById('objectives_blocker2').value;
-    const objectives_blocker3 = document.getElementById('objectives_blocker3').value;
+    console.log(document.getElementById('objectives_blocker1'));
+    const objectives_blocker1 = document.getElementById('objectives_blocker_1').value;
+    const objectives_blocker2 = document.getElementById('objectives_blocker_2').value;
+    const objectives_blocker3 = document.getElementById('objectives_blocker_3').value;
     const voidData = document.getElementById('void').value;
     const layer1 = document.getElementById('layer1').value;
     const pieces = document.getElementById('pieces').value;
